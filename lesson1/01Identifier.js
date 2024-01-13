4edeee30c40cc25c7bfcc3c6f7a6bb776a9fcec9
badsha2756 <badshasaikh95@gmail.com>
2024-01-13 22:53:52 +0530

    Learning identifier

---
+++ lesson1/01Identifier.js
@@ -0,0 +1,16 @@
+/*
+
+Identifier is the name of vairable
+
+1.Identifier should start with a to z , A to Z , _ and $
+2.Identifier can contains numerics like 0 to 9 
+
+*/ 
+
+var name = "Badsha"; //start with a to z
+
+const PI = 3.14; // start with A to Z
+
+let _name = 'Washim'; //start with _
+
+let $name = 'Masud'; //start with $
