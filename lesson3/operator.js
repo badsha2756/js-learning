ce6371bfb70891f6965a19209cb808231788f9ff
badsha2756 <badshasaikh95@gmail.com>
2024-01-14 13:30:13 +0530

    varLetconnsr

---
+++ lesson3/operator.js
@@ -0,0 +1,45 @@
+/* types of oprator:
+   there are deferent types of opertor in javascript.
+      1. Arithmetic operators
+      2.Assignment operators
+      3.Comparison operators
+      4.String operators
+      5. Logical operators
+      6.Bitwise operators
+      7. Ternary operators
+      8.Types operators  */ 
+
+      //Arithmetic operators          
+    let a=3;                          // +  addition
+    let b=8;                         //  - subtraction 
+    let c = a+b;                     // * Multiplecation
+    console.log("Sum : "+ c);        // ** Exponentiation
+                                    // / Division
+                                    // % modulus
+                                     // ++ increment
+                                     // -- decrement
+ //  Asignment Oprator 
+
+  let x= 10;         
+ let y=9;
+
+/* ==  equal to 
+   ===  equal value and equal type 
+   != not equal 
+   > greter than
+   < les than
+   >= greter than or equal to
+   <= less than or equal to 
+   ? ternery opaerator  */
+
+//Logocal operator :
+
+// && logic and 
+// || logic or 
+//  ! logi not
+
+
+
+
+
+                                     
