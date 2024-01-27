2fd629386ea1de510b050b6c953a6d0f3beaf171
badsha2756 <badshasaikh95@gmail.com>
2024-01-27 10:02:16 +0530

    eventlistener

---
+++ Practice_Session/dompractice.js
@@ -0,0 +1,9 @@
+let link =document.getElementsByTagName("a")[0];
+link.innerHTML=" Goto Google";
+link.style.color="green";
+link.style.textDecoration="none"
+link.style.fontSize="2.5rem"
+link.href="Goto google"
+
+let heading1=document.getElementsByTagName("h1")[0];
+heading1.style.color="red";
